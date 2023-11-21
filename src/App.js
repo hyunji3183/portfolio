1a941d90bd45a95c23eee9b0125dd10b837fa98f
hyunji3183 <133857456+hyunji3183@users.noreply.github.com>
2023-11-21 22:25:48 +0900

    “aa”

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Intro_2 from './comp/Intro_2';
 function App() {
     return (
         <>
-            <HashRouter basename='/portfolio'>
+            <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Intro_2 />} />
                     <Route path="/page" element={<Page />} />
@@ -20,7 +20,7 @@ function App() {
                     <Route path="/project" element={<Project />} />
                     <Route path="/contact" element={<Contact />} />
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
         </>
     );
 }
